c7dd81c4d3bf771da21f1a7c8a36af35ee7ce15d
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-11-14 17:34:07 -0300

    Adicionado tratamento de erros e conteudo automatico na classe main

--- src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResource.java
@@ -1,29 +1,34 @@
-package com.udemy.cursomc.resources;
+ package com.udemy.cursomc.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping
+	public ResponseEntity<?> listar() {
+		List<Categoria> categorias = categoriaService.listar();
+		return ResponseEntity.ok().body(categorias);
 	}
 	
+	@GetMapping
+	@RequestMapping(value="/{id}")
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = categoriaService.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
---
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -0,0 +1,28 @@
+package com.udemy.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.repositories.CategoriaRepository;
+import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public List<Categoria> listar() {
+		 return categoriaRepository.findAll();
+	}
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+}
