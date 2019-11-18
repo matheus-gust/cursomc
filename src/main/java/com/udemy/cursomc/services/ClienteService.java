95025793498c6d72a1e1ab96a1f6e6fe1f67e2c6
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-11-18 16:08:08 -0300

    Endpoint/clientes/{id} disponivel

---
+++ src/main/java/com/udemy/cursomc/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.udemy.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.repositories.ClienteRepository;
+import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public List<Cliente> listar() {
+		 return clienteRepository.findAll();
+	}
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
