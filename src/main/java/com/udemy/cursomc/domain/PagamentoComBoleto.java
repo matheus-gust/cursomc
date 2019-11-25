3b8e9d53e7a2ee712d093e238d7ddde09cf1d032
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-11-25 10:33:25 -0300

    end-point Pedido

--- src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
@@ -4,13 +4,19 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.udemy.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
+
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
+	
+
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
 	
 	PagamentoComBoleto() {
---
+++ src/main/java/com/udemy/cursomc/resources/PedidoResource.java
@@ -0,0 +1,34 @@
+ package com.udemy.cursomc.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.cursomc.domain.Pedido;
+import com.udemy.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService pedidoService;
+	
+	@GetMapping
+	public ResponseEntity<?> listar() {
+		List<Pedido> categorias = pedidoService.listar();
+		return ResponseEntity.ok().body(categorias);
+	}
+	
+	@GetMapping
+	@RequestMapping(value="/{id}")
+	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
+		Pedido obj = pedidoService.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
