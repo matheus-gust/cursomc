f61007ba34eccc79be88994ba1d50ec99621ff03
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-11-28 14:53:18 -0300

    Inserindo pedido

--- src/main/java/com/udemy/cursomc/resources/PedidoResource.java
+++ src/main/java/com/udemy/cursomc/resources/PedidoResource.java
@@ -1,15 +1,23 @@
  package com.udemy.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.udemy.cursomc.domain.Categoria;
 import com.udemy.cursomc.domain.Pedido;
+import com.udemy.cursomc.dto.CategoriaDTO;
 import com.udemy.cursomc.services.PedidoService;
 
 @RestController
@@ -20,15 +28,16 @@ public class PedidoResource {
 	private PedidoService pedidoService;
 	
 	@GetMapping
-	public ResponseEntity<?> listar() {
-		List<Pedido> categorias = pedidoService.listar();
-		return ResponseEntity.ok().body(categorias);
-	}
-	
-	@GetMapping
 	@RequestMapping(value="/{id}")
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = pedidoService.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = pedidoService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
