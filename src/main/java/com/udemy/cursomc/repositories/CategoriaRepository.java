a8a3d340c3000dc978a681a9870874f4ce2cbb61
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-11-28 13:20:41 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/udemy/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/udemy/cursomc/repositories/CategoriaRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.udemy.cursomc.domain.Categoria;
-
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-
+	
+	
 }
