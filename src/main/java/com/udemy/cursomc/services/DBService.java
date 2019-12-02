73ec4111783ea86f3aa609779ae2d5cf8748f068
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-12-02 17:45:40 -0300

    Adicionando senha ao cliente

--- src/main/java/com/udemy/cursomc/services/DBService.java
+++ src/main/java/com/udemy/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursomc.domain.Categoria;
@@ -58,6 +59,9 @@ public class DBService {
 
 	@Autowired
 	ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	BCryptPasswordEncoder pe;
 
 	public void instantiateTestDataBase() throws ParseException {
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -117,7 +121,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "matheusgustavo2012@gmail.com", "366556535562", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "matheusgustavo2012@gmail.com", "366556535562", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("33665655", "993838393"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
