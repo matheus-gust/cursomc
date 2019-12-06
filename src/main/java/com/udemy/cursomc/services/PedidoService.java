d322c5334b6570e56a4b66b981618bddeb001221
Matheus Gustavo <matheus_guga2012@hotmail.com>
2019-12-06 17:30:18 -0300

    Cliente so recupera seus pedidos e refresh token

--- src/main/java/com/udemy/cursomc/services/PedidoService.java
+++ src/main/java/com/udemy/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.udemy.cursomc.domain.Cliente;
 import com.udemy.cursomc.domain.ItemPedido;
 import com.udemy.cursomc.domain.PagamentoComBoleto;
 import com.udemy.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.udemy.cursomc.domain.enums.EstadoPagamento;
 import com.udemy.cursomc.repositories.ItemPedidoRepository;
 import com.udemy.cursomc.repositories.PagamentoRepository;
 import com.udemy.cursomc.repositories.PedidoRepository;
+import com.udemy.cursomc.security.UserSS;
+import com.udemy.cursomc.services.exceptions.AuthorizationException;
 import com.udemy.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -68,5 +74,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+ 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+ 		Cliente cliente = clienteService.find(user.getId());
+ 		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 }
